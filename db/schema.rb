69ddee05cb3769677a2ec6950be554502f036c29
Antoine Serval <k6nb4k@gmail.com>
2022-08-30 15:27:14 +0200

    add device on home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_08_30_100340) do
-
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -136,11 +133,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_100340) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
-  add_foreign_key "aromas", "families"
-
   add_foreign_key "alcool_profiles", "users"
-
+  add_foreign_key "aromas", "families"
   add_foreign_key "orders", "spirits"
   add_foreign_key "orders", "users"
   add_foreign_key "reviews", "orders"
