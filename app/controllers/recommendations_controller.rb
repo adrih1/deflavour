c1fbcbc827ed2d9eeba9624808026ae65160a193
Paul Fournier <pfournier597@gmail.com>
2022-09-01 15:47:51 +0200

    recommandation ready

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,5 +1,8 @@
 class RecommendationsController < ApplicationController
   def index
     @recommendations = policy_scope(Recommendation)
+
+    @user = current_user
+    @recommendation = @user.recommendations.first(5)
   end
 end
