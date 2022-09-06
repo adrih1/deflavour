43aedffe53d0509a3beae79c7d0103278be9bb15
Paul Fournier <pfournier597@gmail.com>
2022-09-06 16:18:53 +0200

    stimulus for main chart in place

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -1,17 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
+// Connects to data-controller="chart"
 export default class extends Controller {
-
   connect() {
-    console.log('hello')
-    var ctx = document.getElementById('myChart').getContext('2d');
-    value = document.getElementById("data").innerHTML;
-    value = value.split("-")
-    bobo = document.getElementById("value").innerHTML;
-    data = bobo.split("-")
-    console.log(value)
 
-    var myChart = new Chart(ctx, {
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let value = document.getElementById("data").innerHTML;
+    value = value.split("-")
+    console.log(value)
+    let bobo = document.getElementById("value").innerHTML;
+    let data = bobo.split("-")
+
+    let myChart = new Chart(ctx, {
     type: 'radar',
     data: {
       labels: data,
@@ -27,17 +27,28 @@ export default class extends Controller {
         pointHoverBackgroundColor: '#fff',
         pointHoverBorderColor: 'rgb(255, 99, 132)'
       },],
-      options: {
-      elements: {
-        line: {
-          borderWidth: 3
+
+    },
+
+    options: {
+        elements: {
+          line: {
+            borderWidth: 3
+          }
+        },
+        plugins: {
+          legend: {
+              display: false
+          },
+        },
+        scales: {
+           r: {
+            min: 0,
+            ticks: {
+                display: false // Hides the labels in the middel (numbers)
+            }
+          }
         }
       },
-      plugins: {
-        legend: {
-            display: false
-        },
-    }
-    },
-    },
     });
+  }}
