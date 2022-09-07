2273c94ef8b20302b74de0a02def9731ffd2da3d
Antoine Serval <k6nb4k@gmail.com>
2022-09-07 16:11:10 +0200

    front on the maindashboard

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -25,6 +25,9 @@ application.register("hello", HelloController)
 import MapController from "./map_controller"
 application.register("map", MapController)
 
+import NavlinkController from "./navlink_controller"
+application.register("navlink", NavlinkController)
+
 import NestedFormController from "./nested_form_controller"
 application.register("nested-form", NestedFormController)
 
